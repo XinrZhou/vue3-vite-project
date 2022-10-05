e07de4fcef6d7eaa4ca195c7941670e4c7a64a29
zhouxinrui <1352492197@qq.com>
2022-10-05 15:13:59 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from "path";
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve('./src') // @代替src
+      '@': path.resolve('./src') 
     }
   },
   server: {
@@ -15,7 +15,7 @@ export default defineConfig({
     port: 8080,
     proxy: {
       '/api': {
-        target: 'http://114.116.203.165:8855/api',	//实际请求地址
+        target: 'http://114.116.203.165:8855/api',	
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       },
